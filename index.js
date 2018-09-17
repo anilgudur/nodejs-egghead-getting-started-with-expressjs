26d62028cf0bf0cb28bbbed1a45d04b14e1c523e
anilgudur <anilg.cse@gmail.com>
2018-09-17 12:38:54 +0530

    02-egghead-node-js-getting-started-with-express-routing-basics

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 var express = require('express')
 var app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!!')
+var fs = require('fs')
+var _ = require('lodash')
+var users = []
+
+fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
+  if (err) throw err
+
+  JSON.parse(data).forEach(user => {
+    user.name.full = _.startCase(user.name.first + '  ' + user.name.last)
+    users.push(user)
+  });
 })
 
-app.get('/yo', (req, res) => {
-  res.send('Yo!')
+app.get('/', (req, res) => {
+  var buffer = ''
+  users.forEach(user => {
+    buffer += '<a href="'+ user.username +'">' + user.name.full + '</a><br/>'
+  })
+  res.send(buffer)
+})
+
+app.get(/big.*/, (req, res, next) => {
+  console.log('BIG USER ACCESS')
+  next()
+})
+
+app.get(/.*dog.*/, (req, res, next) => {
+  console.log('DOGS GO WOOF')
+  next()
+})
+
+app.get('/:username', (req, res) => {
+  var username = req.params.username
+  res.send(username)
 })
 
 var server = app.listen(3000, () => {
