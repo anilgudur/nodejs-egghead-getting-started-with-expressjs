fbf6f8c77d18c4ec966df0b36d32c09f46e168b0
anilgudur <anilg.cse@gmail.com>
2018-09-18 02:04:01 +0530

    09-middleware

--- username.js
+++ username.js
@@ -4,12 +4,19 @@ var fs = require('fs')
 
 var router = express.Router({ mergeParams: true })
 
-router.all('/', (req, res, next) => {
-  console.log(req.method, 'for', req.params.username)
+router.use((req, res, next) => {
+  console.log(req.method, 'for', req.params.username + ' at ' + req.path)
   next()
 })
 
-router.get('/', helpers.verifyUser, (req, res) => {
+
+// router.all('/', (req, res, next) => {
+//   console.log(req.method, 'for', req.params.username)
+//   next()
+// })
+
+//router.get('/', helpers.verifyUser, (req, res) => {
+router.get('/', (req, res) => {
   var username = req.params.username
   ////res.send(username)
   //res.render('user', {username: username})
@@ -17,6 +24,14 @@ router.get('/', helpers.verifyUser, (req, res) => {
   var user = helpers.getUser(username)
   res.render('user', { user: user, address: user.location })
 })
+
+
+//#Middleware for error handling
+router.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 
 router.put('/', (req, res) => {
   var username = req.params.username
